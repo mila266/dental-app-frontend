39f3bb909e83637523716fd4257775ba061fd6e2
mila266 <mila.ncaceres.a@gmail.com>
2026-06-27 00:42:07 -0500

    Fix: Resolviendo problema al cargar página

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import AgendarCita from './pages/AgendarCita'
 import Login from './pages/Login'
@@ -7,9 +7,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/agendar-cita" element={<AgendarCita />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   )
